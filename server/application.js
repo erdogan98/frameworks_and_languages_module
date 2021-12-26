4e088f04d11f2b6f3c097d4f2d0bf8c7cfa8b0ae
Erdogan Kervanli <ekervanli@outlook.com>
2021-12-26 19:12:06 +0000

    working on it

--- server/application.js
+++ server/application.js
@@ -72,69 +72,6 @@ app.get('/items/:itemId', (req, res) =>{
         
     }
 });
-
-app.post('/item/', (req,res) => {
-    const items = req.body.item;
-    if(!req.body.user_id){
-        res.status(405)
-        res.json({error: "Invalid Input"})
-    }
- /*           items.push(items);
-            return res.send("ITEM has been added ");
-            */
-           console.log("ITEMS: ", req.body)
-           res.status(200);
-    });  
-
-
-//PUT
-app.put('/item/:itemId', (req, res) => {
-    //look up for item
-    //if not exists, <- 404
-    let itemId = req.params.itemId;
-    let user_id = req.body.user_id;
-    let keywords = [req.body.keywords];
-    let description = req.body.description;
-    let lat = req.body.lat;
-    let lon = req.body.lon;
-
-    let putItems= items.findIndex((newItem)=> {
-        return(newItem.id == Number.parseInt(id))
-    })
-
-    console.log(itemIid, req.body, putItems)
-    if(!id) res.status(404).send('No Item has been found 🥶');
-
-    if(putItems < 0){
-        const itm = items[putItems]
-        items.user_id = user_id
-        items.keywords = keywords
-        items.description = description
-        items.image = image
-        items.lat = lat
-        items.lon = lon
-        res.json(itm)
-    }
-    else{
-        res.status(404)
-    }   
-
-})
-/*
-app.delete('item/:itemId',(res,req)=>{
-    let itemId = req.params.id;
-    let index = items.findIndex((newItem)=>{
-        return newItem.id === Number.parseInt(id)
-    })
-
-    if(index >= 0){
-        let itm = newItem[index]
-        itm.splice(index, 1)
-    }
-    else{
-        res.status(400)
-    }
-})*/
 //fire up API
 app.listen(
     //gives info about when the api is ready \|/
